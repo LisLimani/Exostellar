bdcff56ae778f89c7af8783af45798ca7c255ae7
LisLimani <lis.a.limani.ll@gmail.com>
2023-01-18 00:01:53 +0100

    small changes

--- modules/typing_effect.js
+++ modules/typing_effect.js
@@ -1,40 +1,39 @@
 export function setData() {
-    const txt = `This is a simple project demonstrating the usage of various JavaScript functionalities and concepts.<nxL>
+  const txt = `This is a simple project demonstrating the usage of various JavaScript functionalities and concepts.<nxL>
 This project was build using:<nxL>
 HTML 5<nxL>
 CSS 3<nxL>
 BootStrap v5.2.3<nxL>
 JavaSCript ES6<nxL>`;
-    const outputDiv = document.getElementById("about-text");
-    return [txt, outputDiv];
+  const outputDiv = document.getElementById("about-text");
+  return [txt, outputDiv];
 }
 
 function typeText() {
-    const res = setData();
-    const txt = res[0];
-    const outputDiv = res[1];
-    const splittedTxt = txt.split("<nxL>");
-    // let pElements = [];
-    splittedTxt.forEach((item, index) => {
-        const p = document.createElement("p");
-        outputDiv.appendChild(p);
-    });
-    const allParas = outputDiv.querySelectorAll("p");
-    let i = 0;
-    let currentPara = 0;
-    const timerId = setInterval(() => {
-        allParas[currentPara].innerHTML += splittedTxt[currentPara].charAt(i);
-        i++;
-        if (i === splittedTxt[currentPara].length) {
-            currentPara++;
-            i = 0;
-            if (currentPara === splittedTxt.length) {
-                clearInterval(timerId);
-            }
-        }
-    }, 40);
+  const res = setData();
+  const txt = res[0];
+  const outputDiv = res[1];
+  const splittedTxt = txt.split("<nxL>");
+  splittedTxt.forEach(() => {
+    const p = document.createElement("p");
+    outputDiv.appendChild(p);
+  });
+  const allParas = outputDiv.querySelectorAll("p");
+  let i = 0;
+  let currentPara = 0;
+  const timerId = setInterval(() => {
+    allParas[currentPara].innerHTML += splittedTxt[currentPara].charAt(i);
+    i++;
+    if (i === splittedTxt[currentPara].length) {
+      currentPara++;
+      i = 0;
+      if (currentPara === splittedTxt.length) {
+        clearInterval(timerId);
+      }
+    }
+  }, 40);
 }
 
-window.onload = function() {
-    typeText();
+window.onload = function () {
+  typeText();
 };
